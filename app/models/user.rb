925780265005170d3e6f46e7b07b13d56c473883
gangelo <web.gma@gmail.com>
2015-02-10 23:35:42 -0500

    Set up our association between User and UserProfile.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-  validates :password_confirmation, presence: true
+  has_many :user_profiles
 
+  validates :password_confirmation, presence: true
 end
