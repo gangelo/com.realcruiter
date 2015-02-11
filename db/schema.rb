9a7971eee563ab6f1a500f3756960221323cd15c
gangelo <web.gma@gmail.com>
2015-02-10 23:37:18 -0500

    Migrate to create user_profiles table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150211033437) do
+ActiveRecord::Schema.define(version: 20150211040759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "user_profiles", force: :cascade do |t|
+    t.string   "type"
+    t.string   "profile_name"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
