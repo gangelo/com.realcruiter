c8cb47f69c6ef0f29f972a7b66bab508c9985f2b
gangelo <web.gma@gmail.com>
2015-02-11 20:27:50 -0500

    Add a custom Devise-derived falure handler to redirect back to root_path (rather than new_user_session_path) after a failed sign in attempt.

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,9 @@ module ComRealcruiter
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Load our lib path, so that classes like CustomFailure are available 
+    # to our app.
+    config.autoload_paths += %W(#{config.root}/lib)
   end
 end
