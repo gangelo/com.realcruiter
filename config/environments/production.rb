385ffd2428fca6678022bad482eeea3fcaa66c2e
gangelo <web.gma@gmail.com>
2015-02-12 13:51:10 -0500

    Replace config.serve_static_assets with config.serve_static_files, as it is being deprecated in rails 5.0.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,10 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  # config.serve_static_assets = false
+  # Note: config.serve_static_assets is going to be deprecated in rails 5.0,
+  # use config.serve_static_files instead.
+  config.serve_static_files = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
