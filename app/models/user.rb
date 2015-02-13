e310b528a187aa0a516c356c0307ce5e6b4c5b8b
gangelo <web.gma@gmail.com>
2015-02-12 19:59:49 -0500

    Make User#add_profile a little more dynamic by using inflection.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,18 @@ class User < ActiveRecord::Base
   validates :password_confirmation, presence: true
 
   def add_profile(profile_type, params={})
-    case profile_type
-      when :technical_recruiter_profile
-        self.user_profiles << TechnicalRecruiterProfile.create(params)
-    else
-      # TODO: Redirect to error page.
+    # TODO: Tighten up validation.
+
+    raise(ArgumentError, "profile_type must be a String or Symbol") unless profile_type.respond_to?(:empty?) && (profile_type.is_a?(String) || profile_type.is_a?(Symbol))
+    raise(ArgumentError, "profile_type cannot be empty or nil") if profile_type.nil? || profile_type.empty?
+
+    profile_type = profile_type.to_s if profile_type.is_a? Symbol
+
+    begin
+      user_profile = profile_type.classify.constantize.create(params)
+      self.user_profiles << user_profile
+      user_profile
+    rescue
       raise ArgumentError, "Unrecognized profile type encountered: [#{profile_type}]"
     end
   end
