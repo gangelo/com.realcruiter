6e4fb7b9be018e74bcbc70601ef129c70d1271e2
gangelo <web.gma@gmail.com>
2015-03-18 12:20:31 -0400

    Replace User#add_profile with ProfileCreator

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,25 @@
+require 'profile_creator'
+
 class RegistrationsController < Devise::RegistrationsController
  
   def create
     super {|resource| 
       if resource.persisted? 
-        resource.add_profile params[:profile_type].to_sym
+        #resource.add_profile params[:profile_type].to_sym
+
+        begin
+          interactor = ProfileCreator.new(profile_type_params, resource)
+  		    interactor.execute
+        rescue => error
+          logger.info '*** error encountered: ' + error.message + ' ***'
+          raise ArgumentError, "Unable to create user profile: [#{profile_type_params}]"
+        end
       end
     }
+  end
+
+  def profile_type_params
+    params.require(:profile_type)
   end
 
 end 
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,22 +7,4 @@ class User < ActiveRecord::Base
   has_many :user_profiles
 
   validates_presence_of :password_confirmation
-
-  def add_profile(profile_type, params={})
-    # TODO: Check correct number of params passed.
-
-    raise(ArgumentError, "profile_type must be a String or Symbol") unless profile_type.respond_to?(:empty?) && (profile_type.is_a?(String) || profile_type.is_a?(Symbol))
-    raise(ArgumentError, "profile_type cannot be empty or nil") if profile_type.nil? || profile_type.empty?
-
-    profile_type = profile_type.to_s if profile_type.is_a? Symbol
-
-    begin
-      user_profile = profile_type.classify.constantize.create(params)
-      self.user_profiles << user_profile
-      user_profile
-    rescue => error
-      logger.info '*** error encountered: ' + error.message + ' ***'
-      raise ArgumentError, "Unrecognized profile type encountered: [#{profile_type}]"
-    end
-  end
 end
