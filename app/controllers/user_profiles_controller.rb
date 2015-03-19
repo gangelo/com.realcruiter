eb3d9bd20ec6ad97de6c69f3af513bf7b0ce461c
gangelo <web.gma@gmail.com>
2015-03-19 13:25:24 -0400

    Add edit icon to user profile list view [Finishes #89843966]

--- app/controllers/user_profiles_controller.rb
+++ app/controllers/user_profiles_controller.rb
@@ -7,12 +7,15 @@ class UserProfilesController < ApplicationController
     @user_profiles = UserProfile.where(user_id: current_user.id).order(:profile_name)
   end
 
+  def edit
+  end
+
 	def new
     @user_profile = UserProfile.new({profile_type: :user_profile})
 	end
 
   def create
-    interactor = ProfileCreator.new(user_profile_params, current_user)
+    interactor = ProfileCreator.new(create_params, current_user)
     if interactor.execute
       redirect_to(dashboards_url, notice: 'Your new profile has been created!') and return
     else
@@ -22,7 +25,7 @@ class UserProfilesController < ApplicationController
   end
 
   def destroy
-    current_user.user_profiles.find(params[:profile_id]).delete
+    current_user.user_profiles.find(destroy_params).delete
     redirect_to profiles_url, notice: 'Profile deleted'
   end
 
@@ -38,11 +41,15 @@ class UserProfilesController < ApplicationController
 
   private
 
-  def user_profile_params
+  def create_params
     profile_type = :user_profile if params.has_key? :user_profile
     profile_type = :technical_recruiter_profile if params.has_key? :technical_recruiter_profile
     profile_type = :software_developer_profile if params.has_key? :software_developer_profile
     params.require(profile_type).permit(:profile_name, :profile_type, skills_attributes: [:id, :name])
+  end
+
+  def destroy_params
+    params.require(:profile_id)
   end
 
   def skills_search_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   get '/profiles/skills.json', to: 'user_profiles#skills', as: :user_profile_skills
 
   delete '/profiles/destroy', to: 'user_profiles#destroy', as: :destroy_profile
-
-  resources :user_profiles, path: :profiles, only: [:index, :new, :create], as: :profiles
-  #resources :user_profiles, path: :profiles, as: :profiles
+  resources :user_profiles, path: :profiles, only: [:index, :new, :create, :edit], as: :profiles
   
 
   get '/dashboards', to: 'dashboards#index'
