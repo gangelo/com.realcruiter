0bf48bd0559724c6ad3f2ae270e730881d8942b4
gangelo <web.gma@gmail.com>
2015-04-06 23:12:48 -0400

    Change the default page size for searches from 20 to 15 result rows

--- app/models/search_criteria.rb
+++ app/models/search_criteria.rb
@@ -29,7 +29,7 @@ class SearchCriteria
     @user_profiles = UserProfile.none
     super
 
-    self.class.default_per_page = 20
+    self.class.default_per_page = 15
 
     run_callbacks :initialize do
     end
