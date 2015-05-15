6c3d762814362976564ae6e6993cfc13bdc0c5b3
gangelo <web.gma@gmail.com>
2015-05-14 22:30:30 -0400

    Fix invalid inverse_of clause on :through association

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -7,7 +7,7 @@ class UserProfile < ActiveRecord::Base
   belongs_to :user
 
   has_many :user_profile_skills
-  has_many :skills, through: :user_profile_skills, inverse_of: :user_profile
+  has_many :skills, through: :user_profile_skills
 
   has_many :custom_skills, dependent: :delete_all
 
